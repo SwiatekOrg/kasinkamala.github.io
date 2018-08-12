b15be0c4493cb3b81911b1bb52eebcce412ea584
SwiatekOrg <31080298+SwiatekOrg@users.noreply.github.com>
2018-08-12 15:27:04 +0200

    AddJQuery

---
+++ plik.js
@@ -0,0 +1,13 @@
+$(window).scroll(function(e) {
+     var currentposition = $(document).scrollTop();//aktualna pozycja względem początku          
+     var bannerheight = $("#nav").height();//wysokość elementu, po którym menu ma stać się białe
+      if(currentposition>bannerheight)//odwrotny warunek
+       {         
+           $("#nav").addClass("navScroll");
+           $("nav>ol>li>a>p").addClass("noShadow");
+       }
+       else{
+       		$("#nav").removeClass("navScroll");
+       		$("nav>ol>li>a>p").removeClass("noShadow");
+       }
+  });
