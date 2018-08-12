305487fef2daeea12233500916095d26c774af93
SwiatekOrg <31080298+SwiatekOrg@users.noreply.github.com>
2018-08-12 22:36:44 +0200

    .

--- plik.js
+++ plik.js
@@ -1,4 +1,5 @@
-jQuery(window).scroll(function(e) {
+var $ = jQuery
+$(window).scroll(function(e) {
      var currentposition = $(document).scrollTop();//aktualna pozycja względem początku          
      var bannerheight = $("#nav").height();//wysokość elementu, po którym menu ma stać się białe
       if(currentposition>bannerheight)//odwrotny warunek
